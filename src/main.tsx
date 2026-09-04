7af447d011fd2ed054e5931a978754326c631690
fkamliwale-gif <fkamliwale@gmail.com>
2026-09-04 11:54:52 +0530

    Connect TaskFlow frontend to PHP MySQL backend

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { enableBackendSync, hydrateFromMySQL } from "./backendSync";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function startApp() {
+  await hydrateFromMySQL();
+  enableBackendSync();
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+startApp();
