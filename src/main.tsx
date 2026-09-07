a57d27d1c6f2f03f3c30301a62377e17144f3477
fkamliwale-gif <fkamliwale@gmail.com>
2026-09-07 18:51:30 +0530

    Remove localStorage backend sync startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { enableBackendSync, hydrateFromMySQL } from "./backendSync";
 
-async function startApp() {
-  await hydrateFromMySQL();
-  enableBackendSync();
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
-
-startApp();
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
